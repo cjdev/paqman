541b9f9f453e8ef2e4f75dfb6d7105dea0167398
Stuart Penrose <stu@penrose.us>
2014-12-11 07:57:11 -0800

    administrators would show-up twice in the proctors list in the case where they were proctored themselves

--- src/main/scala/com/cj/paqman/http/QualResource.scala
+++ src/main/scala/com/cj/paqman/http/QualResource.scala
@@ -35,7 +35,9 @@ class QualResource (datas:Data, service:Service) extends HttpObject("/api/quals/
     maybeQual match {
       case Some(record) =>
         val qual = record.latest
-        val proctors = datas.users.toSeq().map(_.latest).filter(service.userIsCurrent(_, record)).map(_.id) :+ qual.administrator
+        val currentUsers = datas.users.toSeq().map(_.latest).filter(service.userIsCurrent(_, record))
+        val proctors = (currentUsers.map(_.id) :+ qual.administrator).distinct
+        
         val dto = new QualDto(qual, proctors)
         OK(Json(Jackson.generate(dto)))
       case None => NOT_FOUND
